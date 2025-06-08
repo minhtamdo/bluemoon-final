d7d18a8adddf4fedf59b0d796bc49f3744218fc4
Bảo Hân <Thao.TTP225766@sis.hust.edu.vn>
2025-06-08 19:52:58 +0700

    updated leader functipn

--- core/apps.py
+++ core/apps.py
@@ -1,11 +1,11 @@
 from django.apps import AppConfig
 
-class CoreConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'core'
-
 class ResidentsConfig(AppConfig):
     name = 'residents'
 
     def ready(self):
         import residents.signals
+
+class CoreConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'core'
